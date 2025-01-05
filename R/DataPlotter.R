9286d65e2901a0979c7135b97e3c38f1c1f63b19
olly minns <ojminns@itofinity.co.uk>
2025-01-05 14:52:13 +0000

    Rewritten plotting functions to use new generated datasets in quicker and more efficient manner

--- R/DataPlotter.R
+++ R/DataPlotter.R
@@ -1,68 +1,30 @@
 ## ----simple, echo=TRUE-------------------------------
 
-#make plotting function
-dp.mega_plotter <- function(datasets, xvar, yvar, xlab, ylab, xlim, ylim, title, colour){
-  #initialise plot with point at (0,0)
-  plot(0, 0, xlab = xlab, ylab = ylab, xlim=c(0,xlim), ylim=c(0,ylim),main = title, col = 'red')
-  #iterate through datasets
-  for (name in names(datasets)){
-    #plot points of all datasets in list
-    data = datasets[[name]]
-    points(data[[xvar]], data[[yvar]], col = colour)
-  }
-}
-
-dp.multi_plotter <- function(datasets1, datasets2, datasets3, xvar, yvar, xlab, ylab, xlim, ylim, title, colour1, colour2, colour3){
-  #call megaplotter function to plot first plot
-  dp.mega_plotter(datasets1, xvar, yvar, xlab, ylab, xlim, ylim, title, colour1)
-  #check datasets2 exists
-  if (is.list(datasets2)){
-    #plot 2nd dataset
-    for (name in names(datasets2)){
-      #plot points of all datasets in list
-      data = datasets2[[name]]
-      points(data[[xvar]], data[[yvar]], col = colour2)
+dp.plotter <- function(data, column, xvar, yvar, xlab, ylab, xlim, ylim, title){
+  plot(0,0, xlab=xlab, ylab=ylab, xlim=xlim, ylim=ylim, main=title, col ='white')
+  #find unique values in specified column
+  unique_vals <- unique(data[[column]])
+  #generate colour palette equal to number of unique vals
+  pal <- c(1:length(unique_vals))
+  i <- 1
+  #iterate through unique_vals and plot data
+  for( q in unique_vals){
+    colour <- pal[i]
+    subset <- data[data[, column]==q,]
+    points(subset[[xvar]], subset[[yvar]], col=colour,pch=19, cex=0.65)
+    #line of best fit
+    lmodel <- lm(subset[[yvar]] ~ subset[[xvar]])
+    
+    #ignore line draw if gradient is infinite
+    if(!is.na(coef(lmodel)[2])){
+      b <- coef(lmodel)[2]
+      a <- coef(lmodel)[1]
+      abline(a=a, b=b, col=colour, lty=3, lwd=2)
     }
+    
+    
+    #increment colour
+    i <- i +1
   }
-  else{
-    print("no secondary dataset")
-  }
-  #check datasets3 exists
-  if (is.list(datasets3)){
-    #plot 2nd dataset
-    for (name in names(datasets3)){
-      #plot points of all datasets in list
-      data = datasets3[[name]]
-      points(data[[xvar]], data[[yvar]], col = colour3)
-    }
-  }
-  else{
-    print("no secondary dataset")
-  }
-}
-
-dp.multi_plotter2 <- function(datasets1, oth_datasets, xvar, yvar, xlab, ylab, xlim, ylim, title, colour1, colours){
-  #call megaplotter function to plot first plot
-  dp.mega_plotter(datasets1, xvar, yvar, xlab, ylab, xlim, ylim, title, colour1)
-  #check oth_datasets exists
-  if (is.list(oth_datasets)){
-    # start a counter for i that counts each dataset list read
-    i <- 1
-    #iterate through list of dataset lists
-    for (i in length(oth_datasets)){
-      #extract each dataset list from the list of dataset lists
-      datasets <- oth_datasets[[i]]
-      for (name in names(datasets)){
-        #plot points of all datasets in list
-        data = datasets[[name]]
-        points(data[[xvar]], data[[yvar]], col = colours[i])
-      }
-      #increase i
-      i <- i + 1
-    }
-  }
-  
-  else{
-    print("no secondary dataset")
-  }
+  legend(0.9*xlim[2],ylim[2], legend=unique_vals, col=pal, lty=3, lwd=3, cex=0.8)
 }
