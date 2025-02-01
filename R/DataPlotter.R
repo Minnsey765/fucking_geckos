1610645e33cb5238b1287b5cb164a2cfa5bece87
olly minns <ojminns@itofinity.co.uk>
2025-02-01 19:15:09 +0000

    final flurry of plots and graphs and stuff and random functions like non-parametric stats tests

--- R/DataPlotter.R
+++ R/DataPlotter.R
@@ -77,10 +77,10 @@ dp.cat_plotter <- function(data, categories, xvar, yvar, xlab, ylab, xlim, ylim,
 #boxplot for comparing gecko size in experimental and control quadrats
 dp.boxplot <- function(data, con_var, dis_var, title, xlab, ylab){
   #find unique values in discrete variable column
-  unique_vals <- unique(data[[dis_var]])
+  #unique_vals <- unique(data[[dis_var]])
   #print(cbind(data[[con_var]],data[[dis_var]]))
   #plot box plot
-  box_plot <- boxplot(data[[con_var]]~data[[dis_var]], main=title, cex.main=1, names=unique_vals, xlab = xlab, ylab=paste(ylab,con_var))
+  box_plot <- boxplot(data[[con_var]]~data[[dis_var]], main=title, cex.main=1, xlab = xlab, ylab=paste(ylab,con_var))
   return(boxplot)
 }
 
@@ -109,27 +109,29 @@ dp.t_test <- function(data, con_var, dis_var){
   return(sentence)
 }
 
-#dp.kmeans <- function(data, xvar, yvar, clusters){
+dp.kmeans <- function(data, xvar, yvar, clusters){
   #generate palette of correct number of colours
-#  col_pal <- palette(rainbow(clusters))
+  col_pal <- palette(rainbow(clusters))
   #create column names
-#  names <- c(xvar,yvar)
+  names <- c(xvar,yvar)
   #create new vector matrix for kmeans analysis and remove NAs
-#  dataframe <- na.omit(cbind(data[[xvar]],data[[yvar]]))
-#  km.rs <- kmeans(dataframe, clusters, iter.max = 10, nstart = 1)
+  dataframe <- na.omit(cbind(data[[xvar]],data[[yvar]]))
+  km.rs <- kmeans(dataframe, clusters, iter.max = 10, nstart = 1)
   
   #convert dataframe into actual dataframe and name columns
-#  dataframe <- as.data.frame(dataframe)
-#  colnames(dataframe) <- names
+  dataframe <- as.data.frame(dataframe)
+  colnames(dataframe) <- names
   #plot cluster
-#  plot <- fviz_cluster(km.rs, data = dataframe,
-#               palette = col_pal, 
-#               geom = "point",
-#               ellipse.type = "convex", 
-#               ggtheme = theme_bw()
-#  )
-#  return(plot)
-#}
+  plot <- fviz_cluster(km.rs, data = dataframe,
+               palette = col_pal, 
+               geom = "point",
+               ellipse.type = "convex", 
+               ggtheme = theme_bw()
+  )
+  palette("default")
+  return(plot)
+  
+}
 
 
 # oneway anova test + diagnostic plots
@@ -205,5 +207,127 @@ trip_t_test <- function(data, dis_var, con_var){
 #linear regression model + t statistic
 dp.regression <- function(data, xvar, yvar){
   lmodel <- lm(data[[yvar]] ~ data[[xvar]])
+  plot(lmodel)
   return(summary(lmodel))
+}
+
+dp.scale_reg <- function(data, xvar, yvar){
+  scaled <- as.data.frame(cbind(data[[xvar]],data[[yvar]]))
+  colnames(scaled) <- c("indep", "depen")
+  scaled <- scale(scaled)
+  print(scaled)
+  lmodel <- lm(scaled[[indep]] ~ scaled[[depen]])
+  plot(lmodel)
+  return(summary(lmodel))
+}
+
+dp.trip_kruskal <- function(data, dis_var, con_var){
+  unique_vals <- unique(data[[dis_var]])
+  for(i in 1:length(unique_vals)){
+    #omit one value type in discrete column from dataset
+    subset <- data[data[, dis_var] != unique_vals[i],]
+    #perform t test
+    kw_test <- kruskal.test(subset[[con_var]] ~ subset[[dis_var]], subset)
+    result <- ""
+    if(kw_test$p.value > 0.05){
+      result <- "no"
+    }
+    else{
+      result <- "a significant"
+    }
+    #mention what categories are being tested
+    phrase <- ""
+    if(i == 1){
+      phrase <- paste(unique_vals[i+1], "&", unique_vals[i+2])
+    }
+    else if(i == 2){
+      phrase <- paste(unique_vals[i-1], "&", unique_vals[i+1])
+    }
+    else{
+      phrase <- paste(unique_vals[i-2], "&", unique_vals[i-1])
+    }
+    print(kw_test)
+    sentence <- paste("There is ", result, " difference between the treatments. (", dis_var, " ", phrase, ") (p value =", kw_test$p.value, ")")
+    print(sentence)
+  }
+}
+
+
+dp.kruskal <- function(data, con_var, dis_var){
+  kw_test <- kruskal.test(data[[con_var]] ~ data[[dis_var]], data)
+  result <- ""
+  if(kw_test$p.value > 0.05){
+    result <- "no"
+  }
+  else{
+    result <- "a significant"
+  }
+  print(kw_test)
+  sentence <- paste("There is ", result, " difference between each treatment. (p value =", kw_test$p.value, ")")
+  return(sentence)
+}
+
+#spearmens rank
+
+dp.spearman <- function(data, xvar, yvar){
+  corr <- cor.test(data[[xvar]], data[[yvar]], method="spearman")
+  print(corr)
+  if(corr$p.value > 0.05){
+    result <- "no"
+  }
+  else{
+    result <- "a significant"
+  }
+  sentence <- paste("There is ", result, " correllation between each treatment. (p value =", corr$p.value, ")")
+  return(sentence)
+}
+
+dp.gekdate_plotter <- function(data, zone, yvar, time, xlab, ylab, xlim, ylim, title){
+  #xvariable always date here
+  xvar <- "date"
+  #remove all rows in wrong zone and time
+  subset <- na.omit(data[data$zone==zone,])
+  subset <- subset[subset$time==time,]
+  #Create new dataframe with total number of geckos within each zone on a given date
+  sum_11th <- 0
+  sum_12th <- 0
+  sum_13th <- 0
+  sum_14th <- 0
+  for(i in 1:length(subset[[xvar]])){
+    if(subset[[xvar]][i] == "11/09/2024"){
+      sum_11th <- sum_11th + subset[[yvar]][i]
+    }
+    else if(subset[[xvar]][i] == "12/09/2024"){
+      sum_12th <- sum_12th + subset[[yvar]][i]
+    }
+    else if(subset[[xvar]][i] == "13/09/2024"){
+      sum_13th <- sum_13th + subset[[yvar]][i]
+    }
+    else if(subset[[xvar]][i] == "14/09/2024"){
+      sum_14th <- sum_14th + subset[[yvar]][i]
+    }
+  }
+  dates <- c(11,12,13,14)
+  abundance <- c(sum_11th,sum_12th,sum_13th,sum_14th)
+  new_data <- as.data.frame(cbind(dates,abundance))
+  colnames(new_data) <- c("dates", "abundance")
+  #boxplot <- boxplot(data[[yvar]]~data$date)#, main=title, cex.main=1, names=unique_vals, xlab = xlab, ylab=paste(ylab,con_var))
+  #plot <- dp.cat_plotter(subset, category, xvar, yvar, xlab, ylab, xlim, ylim, title)
+  plot <- plot(new_data$dates, new_data$abundance, xlab=xlab, ylab=ylab, xlim=xlim, ylim=ylim, main=title)
+  return(plot)
+}
+
+
+dp.mannwhitney <- function(data, con_var, dis_var){
+  mwu_test <- wilcox.test(data[[con_var]] ~ data[[dis_var]], data)
+  result <- ""
+  if(mwu_test$p.value > 0.05){
+    result <- "no"
+  }
+  else{
+    result <- "a significant"
+  }
+  print(mwu_test)
+  sentence <- paste("There is ", result, " difference between each treatment. (p value =", mwu_test$p.value, ")")
+  return(sentence)
 }
